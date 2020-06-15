0953de07c6aea9c214ee55f10b65e285862f42d2
Ivan Kondratiuk <ivan.kondratiuk@bp.com>
2020-06-15 13:15:28 +0300

    formatting

--- domain/entity/board.go
+++ domain/entity/board.go
@@ -2,7 +2,7 @@ package entity
 
 // Board represents the board entity stored in repository
 type Board struct {
-	Id      int64  `json:"id"`
-	Name    string `json:"name"`
+	Id      int64    `json:"id"`
+	Name    string   `json:"name"`
 	Columns []Column `json:"columns"`
 }
