0afe3c60ac016942ec698cad561a2ffde23c8592
Ivan Kondratiuk <ivan.kondratiuk@bp.com>
2020-06-24 23:53:37 +0300

    add db, db configs

--- main.go
+++ main.go
@@ -1,9 +1,45 @@
 package main
 
 import (
+	"log"
+	"strconv"
+
+	"github.com/spf13/viper"
+
 	"github.com/i1kondratiuk/kanban/application"
+	"github.com/i1kondratiuk/kanban/config"
+	"github.com/i1kondratiuk/kanban/domain/repository"
+	"github.com/i1kondratiuk/kanban/infrastructure/persistence"
 	"github.com/i1kondratiuk/kanban/interface/web/webjson/handler"
 )
+
+func init() {
+	// Loading environmental variables
+	viper.SetConfigName("config")
+	viper.AddConfigPath(".")
+	var configuration config.Configuration
+
+	if err := viper.ReadInConfig(); err != nil {
+		log.Fatalf("Error reading config file, %s", err)
+	}
+	err := viper.Unmarshal(&configuration)
+	if err != nil {
+		log.Fatalf("Unable to decode into struct, %v", err)
+	}
+
+	db, err := persistence.NewDbConnection(
+		configuration.Database.Host,
+		strconv.Itoa(int(configuration.Database.Port)),
+		configuration.Database.User,
+		configuration.Database.Password,
+		configuration.Database.Name,
+		configuration.Database.Driver,
+	)
+
+	if err != nil {
+		panic(err.Error())
+	}
+}
 
 func main() {
 	application.InitBoardManagerApp(&application.BoardManagerAppImpl{})
