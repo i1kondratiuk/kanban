e525ce3e35ff68c2e63e2b06011a3493d990a9a2
Ivan Kondratiuk <ivan.kondratiuk@bp.com>
2020-06-28 00:02:22 +0300

    add webjson json interface models

---
+++ interface/web/webjson/model/board.go
@@ -0,0 +1,10 @@
+package entity
+
+import (
+	"github.com/i1kondratiuk/kanban/application/model"
+)
+
+// Board represents the model to be shown to the API client
+type Board struct {
+	model.Board
+}
---
+++ interface/web/webjson/model/column.go
@@ -0,0 +1,10 @@
+package entity
+
+import (
+	"github.com/i1kondratiuk/kanban/application/model"
+)
+
+// Column represents the model to be shown to the API client
+type Column struct {
+	model.Column
+}
